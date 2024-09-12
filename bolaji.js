c32b2a65dc988ac57fb564d4a9084ccbb7f17cfe
Rampop01 <popoolarahmat96@gmail.com>
2024-09-12 01:41:57 +0100

    implemented the light and dark mode,user question is saved on refresh

---
+++ bolaji.js
@@ -0,0 +1,25 @@
+// const getChatResponse = async () => {
+//   const API_URL = "";
+
+//   // const requestOptions = {
+//   //   method: "POST",
+//   //   headers: {
+//   //     "Content-Type": "application/json",
+//   //     Authorization: `Bearer ${}`,
+//   //   },
+//   //   body: JSON.stringify({
+//   //     model: "gpt-3.5-turbo-instruct",
+//   //     prompt: userText,
+//   //     max_tokens: 7,
+//   //     temperature: 0,
+//   //     n: 1,
+//   //     stop: null,
+//   //   }),
+//   // };
+//   try {
+//     const response = await (await fetch(API_URL, requestOptions)).json();
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
